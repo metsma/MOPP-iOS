d1a0c7648c2e954465051385bd380ada8e31e8f5
Raul Metsma <raul@metsma.ee>
2025-05-13 10:55:41 +0300

    tmp

--- MoppApp/MoppApp/CryptoContainerViewController.swift
+++ MoppApp/MoppApp/CryptoContainerViewController.swift
@@ -55,6 +55,7 @@ class CryptoContainerViewController : ContainerViewController, CryptoActions {
     deinit {
         NotificationCenter.default.removeObserver(self)
         printLog("Deinit CryptoContainerViewController")
+        MoppFileManager.removeFiles()
     }
 }
 
